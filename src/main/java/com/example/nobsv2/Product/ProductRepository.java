51d9610ba2a666443a984992a4d02fec6faebc0d
Sadiqali Shaik <sadiq2001ali@gmail.com>
2025-05-07 03:17:17 +0530

    Added Search functionality using JPA
    Added SearchProductService class

--- src/main/java/com/example/nobsv2/Product/ProductRepository.java
+++ src/main/java/com/example/nobsv2/Product/ProductRepository.java
@@ -2,8 +2,18 @@ package com.example.nobsv2.Product;
 
 import com.example.nobsv2.Product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+    //spring data jpa
+    List<Product> findByNameContaining(String name);
+
+    //JPQL
+    @Query("SELECT p FROM Product p WHERE p.name LIKE %:keyword% OR p.description LIKE %:keyword%")
+    List<Product> findByNameOrDescriptionContaining(@Param("keyword")String name);
 
 }
---
+++ src/main/java/com/example/nobsv2/Product/services/SearchProductService.java
@@ -0,0 +1,26 @@
+package com.example.nobsv2.Product.services;
+
+import com.example.nobsv2.Product.ProductRepository;
+import com.example.nobsv2.Product.model.ProductDTO;
+import com.example.nobsv2.Query;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SearchProductService implements Query<String, List<ProductDTO>> {
+    private final ProductRepository productRepository;
+
+    public SearchProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public ResponseEntity<List<ProductDTO>> execute(String name) {
+        return ResponseEntity.ok(productRepository.findByNameOrDescriptionContaining(name)
+                .stream()
+                .map(ProductDTO::new)
+                .toList());
+    }
+}
